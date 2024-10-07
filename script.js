b944a77963221dc026d36b4a611a16322e8b471d
Harry <harry_woodgate11@hotmail.co.uk>
2024-10-07 12:28:17 +0100

    Add functions in gamecontrol to check for winner, however with bug on checking for winner via diagonals where it alerts multiple times.

--- script.js
+++ script.js
@@ -4,10 +4,43 @@ const gameBoard = {
 }
 
 const gameControl = {
+    checkVerticalRows : function (board) {
+        if (board[0] === board[3] && board[6]) {
+            alert("Winner");
+        }
+        else if (board[1] === board[4] && board[7]) {
+            alert("Winner");
+        }
+        else if (board[2] === board[5] && board[8]) {
+            alert("Winner");
+        }
+    },
+    checkHorizontalRows : function (board) {
+        if (board[0] === board[1] && board[2]) {
+            alert("Winner");
+        }
+        else if (board[3] === board[4] && board[5]) {
+            alert("Winner");
+        }
+        else if (board[6] === board[7] && board[8]) {
+            alert("Winner");
+        }
+    },
+    checkDiagonalRows : function (board) {
+        if (board[0] === board[4] && board[8]) {
+            console.log("Winner");
+        }
+        else if (board[2] === board[4] && board[6]) {
+            alert("Winner");
+        }
+    },
     setBoard : function (input, square) {
         gameBoard.board.splice(square, 1, input);
         console.log(gameBoard.board);
-    }
+        this.checkVerticalRows(gameBoard.board);
+        this.checkHorizontalRows(gameBoard.board);
+        this.checkDiagonalRows(gameBoard.board);
+    },
 }
 
 function createPlayer (name, input) {
