bc71d272929ddf942282167086e195e2ba5361b7
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2024-10-08 11:10:56 +0100

    Add checkWinner and check for draw functions.

--- script.js
+++ script.js
@@ -1,48 +1,74 @@
 
 const gameBoard = (function () {
-   let board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+   let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
    return {board};
 })();
 
-const gameControl = {
-    checkVerticalRows : function (board) {
+const gameControl = (function () {
+
+    let turnCount = 0;
+
+    const checkWinner = function(index) {
+        if (gameBoard.board[index] === "X") {
+            console.log("You Win " + playerOne.name);
+            turnCount = 0;
+            gameBoard.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+        }
+        else {
+            console.log("You Win" + playerTwo.name);
+            turnCount = 0;
+            gameBoard.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+        }
+    }
+
+    const checkVerticalRows = function (board) {
         if ((board[0] === board[3]) && (board[0] === board[6])) {
-            console.log("Winner");
+            checkWinner(0);
         }
         else if ((board[1] === board[4]) && (board[1] === board[7])) {
-            console.log("Winner");
+            checkWinner(1);
         }
         else if ((board[2] === board[5]) && (board[2] === board[8])) {
-            console.log("Winner");
+            checkWinner(2);
         }
-    },
-    checkHorizontalRows : function (board) {
+    }
+    const checkHorizontalRows = function (board) {
         if ((board[0] === board[1]) && (board[0] === board[2])) {
-            console.log("Winner");
+            checkWinner(0);
         }
         else if ((board[3] === board[4]) && (board[3] === board[5])) {
-            console.log("Winner");
+            checkWinner(3);
         }
         else if ((board[6] === board[7]) && (board[6] === board[8])) {
-            console.log("Winner");
+            checkWinner(6);
         }
-    },
-    checkDiagonalRows : function (board) {
+    }
+    const checkDiagonalRows = function (board) {
         if ((board[0] === board[4]) && (board[0] === board[8])) {
-            console.log("Winner");
+            checkWinner(0);
         }
         else if ((board[2] === board[4]) && (board[2] === board[6])) {
-            console.log("Winner");
+            checkWinner(2);
         }
-    },
-    setBoard : function (input, square) {
+    }
+    const checkForDraw = function (board) {
+        if (turnCount === 9) {
+            console.log("Its a draw");
+        }
+    }
+    const setBoard = function (input, square) {
         gameBoard.board.splice(square, 1, input);
         console.log(gameBoard.board);
-        this.checkVerticalRows(gameBoard.board);
-        this.checkHorizontalRows(gameBoard.board);
-        this.checkDiagonalRows(gameBoard.board);
-    },
-}
+        checkVerticalRows(gameBoard.board);
+        checkHorizontalRows(gameBoard.board);
+        checkDiagonalRows(gameBoard.board);
+        turnCount++;
+        checkForDraw(gameBoard.board);
+    }
+
+    return{setBoard};
+
+})();
 
 function createPlayer (name, input) {
     const takeTurn = function (square) {gameControl.setBoard(input, square);}
@@ -52,4 +78,4 @@ function createPlayer (name, input) {
 const playerOne = createPlayer("Harry", "X");
 const playerTwo = createPlayer("Bob", "O");
 
-playerOne.takeTurn(0);
+
