e6755da98685157d4f40ede9fb58f9285a38d62b
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2024-10-08 11:30:16 +0100

    Modify setBoard to function to not allow 2 moves in the same place.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const gameControl = (function () {
 
     let turnCount = 0;
 
-    const checkWinner = function(index) {
+    const winSequence = function(index) {
         if (gameBoard.board[index] === "X") {
             console.log("You Win " + playerOne.name);
             turnCount = 0;
@@ -23,32 +23,32 @@ const gameControl = (function () {
 
     const checkVerticalRows = function (board) {
         if ((board[0] === board[3]) && (board[0] === board[6])) {
-            checkWinner(0);
+            winSequence(0);
         }
         else if ((board[1] === board[4]) && (board[1] === board[7])) {
-            checkWinner(1);
+            winSequence(1);
         }
         else if ((board[2] === board[5]) && (board[2] === board[8])) {
-            checkWinner(2);
+            winSequence(2);
         }
     }
     const checkHorizontalRows = function (board) {
         if ((board[0] === board[1]) && (board[0] === board[2])) {
-            checkWinner(0);
+            winSequence(0);
         }
         else if ((board[3] === board[4]) && (board[3] === board[5])) {
-            checkWinner(3);
+            winSequence(3);
         }
         else if ((board[6] === board[7]) && (board[6] === board[8])) {
-            checkWinner(6);
+            winSequence(6);
         }
     }
     const checkDiagonalRows = function (board) {
         if ((board[0] === board[4]) && (board[0] === board[8])) {
-            checkWinner(0);
+            winSequence(0);
         }
         else if ((board[2] === board[4]) && (board[2] === board[6])) {
-            checkWinner(2);
+            winSequence(2);
         }
     }
     const checkForDraw = function (board) {
@@ -57,13 +57,19 @@ const gameControl = (function () {
         }
     }
     const setBoard = function (input, square) {
+        if (typeof(gameBoard.board[square]) === "string") {
+            console.log("Already moved there!");
+            return;
+        }
+        else {
         gameBoard.board.splice(square, 1, input);
         console.log(gameBoard.board);
         checkVerticalRows(gameBoard.board);
         checkHorizontalRows(gameBoard.board);
         checkDiagonalRows(gameBoard.board);
         turnCount++;
-        checkForDraw(gameBoard.board);
+        checkForDraw(gameBoard.board); 
+        }
     }
 
     return{setBoard};
