07d400529a7fdba120c1167d90b7141ceab27be1
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2024-10-08 10:34:36 +0100

    Fix the check winner bug for all checks.

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 
-const gameBoard = {
-    board : [1, 2, 3, 4, 5, 6, 7, 8, 9]
-}
+const gameBoard = (function () {
+   let board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+   return {board};
+})();
 
 const gameControl = {
     checkVerticalRows : function (board) {
-        if (board[0] === board[3] && board[6]) {
+        if ((board[0] === board[3]) && (board[0] === board[6])) {
             console.log("Winner");
         }
-        else if (board[1] === board[4] && board[7]) {
+        else if ((board[1] === board[4]) && (board[1] === board[7])) {
             console.log("Winner");
         }
-        else if (board[2] === board[5] && board[8]) {
-            console.log("WinnerV");
+        else if ((board[2] === board[5]) && (board[2] === board[8])) {
+            console.log("Winner");
         }
     },
     checkHorizontalRows : function (board) {
-        if (board[0] === board[1] && board[2]) {
+        if ((board[0] === board[1]) && (board[0] === board[2])) {
             console.log("Winner");
         }
-        else if (board[3] === board[4] && board[5]) {
+        else if ((board[3] === board[4]) && (board[3] === board[5])) {
             console.log("Winner");
         }
-        else if (board[6] === board[7] && board[8]) {
-            console.log("WinnerH");
+        else if ((board[6] === board[7]) && (board[6] === board[8])) {
+            console.log("Winner");
         }
     },
     checkDiagonalRows : function (board) {
         if ((board[0] === board[4]) && (board[0] === board[8])) {
-            console.log("WinnerD");
+            console.log("Winner");
         }
-        else if (board[2] === board[4] && board[6]) {
+        else if ((board[2] === board[4]) && (board[2] === board[6])) {
             console.log("Winner");
         }
     },
@@ -52,5 +53,3 @@ const playerOne = createPlayer("Harry", "X");
 const playerTwo = createPlayer("Bob", "O");
 
 playerOne.takeTurn(0);
-playerOne.takeTurn(4);
-playerOne.takeTurn(8);
