8a0751e907bfe6e43b4bf935c8f2320bea4687a8
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2024-10-16 15:13:28 +0100

    Edit resetBoard function so that it resets game display.

--- script.js
+++ script.js
@@ -125,6 +125,7 @@ const dom = (function () {
     const resetBoard = () => {
         game.turnCount = 0;
         game.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+        cacheDom.gameStateDisplay.textContent = "";
     }
 
     const displayWinner = function(index) {
